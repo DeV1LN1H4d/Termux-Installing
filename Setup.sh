aab3c96bfd328bd7d4bdd90b329c611279a10dc8
DEVIL NIHAD <88210650+DeV1LN1H4d@users.noreply.github.com>
2021-12-19 15:02:30 +0600

    Add files via upload

---
+++ Setup.sh
@@ -0,0 +1,56 @@
+pkg update && pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install git -y
+pkg install figlet -y
+pip install --upgrade pip
+pkg install cmatrix -y
+pkg install toilet -y
+pkg install nano -y
+pkg install php 
+pkg install pip -y
+pkg install pip2
+pip2 install requests
+pip install future
+pip2 install requirements
+apt install ruby
+apt install openssh
+apt install wget
+apt install curl
+apt install proof
+termux-setup-storage
+pip install --upgrade pip
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+pkg install git python -y
+pip install aniso8601
+pip install beautifulsoup4
+pip install bs4
+pip install cached-property
+pip install certifi
+pip install charset-normalizer
+pip install click
+pip install greenlet
+pip install html5lib
+pip install idna
+pip install itsdangerous
+pip install Jinja2
+pip install MarkupSafe
+pip install msgpack
+pip install pycurl
+pip install pytz
+pip install requests
+pip install six
+pip install --upgrade pip
+pip install soupsieve
+pip install spark-parser
+pip install urllib3
+pip install webencodings
+pip instspark-parser
+pip install --upgrade pip
+pip install xdis
+pip install futures
+pip2 install requests
+pip install --upgrade pip
